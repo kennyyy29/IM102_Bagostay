fcad3262c88c86076d633e545078f6935c856766
John Kenneth Bagotsay <kinnethbagotsay@gmail.com>
2025-09-15 20:29:34 +0800

    My SW1

---
+++ SW1/sitWork1prob1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class program
+{
+    static string PrimeOrComposite(int num)
+    {
+        if (num <= 1)
+            return "Composite"; 
+
+        for (int i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0) 
+                return "Composite";
+        }
+
+        return "Prime";
+    }
+
+    static void Main()
+    {
+        int number = 5;
+        int count = 1;
+        int[] num = new int[number];
+        for (int i = 0; i < number; i++)
+        {
+            Console.Write($"Enter number {i + 1}: ");
+            num[i] = int.Parse(Console.ReadLine());
+
+           
+            count++;
+        }
+        foreach (int ram in num)
+        {
+            string result = PrimeOrComposite(ram);
+
+            Console.WriteLine($"{count}. {ram,-5} {result}");
+        }
+        Console.ReadKey();
+    }
+}
+
+
+
+
+
---
+++ SW1/sitWork1prob2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sitwork1prob2
+{
+    class Program
+    {
+        static void ConvertCurrency(double dollar)
+        {
+            double peso = dollar * 57.17;   
+            double yen = dollar * 146.67;    
+
+            Console.WriteLine();
+            Console.WriteLine($"{"Dollar($)",-15}{"Phil Peso(P)",-15}{"Jpn Yen(¥)",-15}");
+            Console.WriteLine($"{dollar,-15:N0}{peso,-15:N2}{yen,-15:N2}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a value [$] ): ");
+            string input = Console.ReadLine();
+
+            input = input.Replace(",", "");
+
+            double dollar = double.Parse(input);
+
+           
+            ConvertCurrency(dollar);
+
+            Console.ReadKey();
+
+        }
+    }
+}
